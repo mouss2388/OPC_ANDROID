6fae3daeaab23819594103124dbab492d3b4482f
mouss2388 <mouss2388@gmail.com>
2021-12-24 17:35:02 +0100

    Update Test

--- Projet_3/Entrevoisins/app/src/androidTest/java/com/openclassrooms/entrevoisins/neighbour_list/NeighboursListTest.java
+++ Projet_3/Entrevoisins/app/src/androidTest/java/com/openclassrooms/entrevoisins/neighbour_list/NeighboursListTest.java
@@ -3,13 +3,13 @@ package com.openclassrooms.entrevoisins.neighbour_list;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.action.ViewActions.swipeLeft;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.assertThat;
 import static android.support.test.espresso.matcher.ViewMatchers.hasChildCount;
-import static android.support.test.espresso.matcher.ViewMatchers.hasDescendant;
 import static android.support.test.espresso.matcher.ViewMatchers.hasMinimumChildCount;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static com.openclassrooms.entrevoisins.utils.RecyclerViewItemCountAssertion.withItemCount;
 import static org.hamcrest.Matchers.not;
@@ -18,20 +18,25 @@ import static org.hamcrest.core.IsNull.notNullValue;
 
 import android.support.test.espresso.UiController;
 import android.support.test.espresso.ViewAction;
+import android.support.test.espresso.ViewInteraction;
 import android.support.test.espresso.contrib.RecyclerViewActions;
 import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
 
 import com.openclassrooms.entrevoisins.R;
 import com.openclassrooms.entrevoisins.controller.ListNeighbourActivity;
 import com.openclassrooms.entrevoisins.utils.DeleteViewAction;
 
+import org.hamcrest.Description;
 import org.hamcrest.Matcher;
+import org.hamcrest.Matchers;
+import org.hamcrest.TypeSafeMatcher;
 import org.junit.Before;
 import org.junit.FixMethodOrder;
-import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -66,7 +71,7 @@ public class NeighboursListTest {
     @Test
     public void myNeighboursList_shouldNotBeEmpty() {
         // First scroll to the position that needs to be matched and click on it.
-        onView(allOf(ViewMatchers.withId(R.id.list_neighbours), hasDescendant(withText("Vincent"))))
+        onView(allOf(ViewMatchers.withId(R.id.list_neighbours), isDisplayed()))
                 .check(matches(hasMinimumChildCount(2)));
 
     }
@@ -77,19 +82,19 @@ public class NeighboursListTest {
     @Test
     public void myNeighboursList_deleteAction_shouldRemoveItem() {
         // Given : We remove the element at position 2
-        onView(allOf(ViewMatchers.withId(R.id.list_neighbours), hasDescendant(withText("Vincent")))).check(withItemCount(ITEMS_COUNT));
+        onView(allOf(ViewMatchers.withId(R.id.list_neighbours), isDisplayed())).check(withItemCount(ITEMS_COUNT));
         // When perform a click on a delete icon
-        onView(allOf(ViewMatchers.withId(R.id.list_neighbours), hasDescendant(withText("Vincent"))))
+        onView(allOf(ViewMatchers.withId(R.id.list_neighbours), isDisplayed()))
                 .perform(RecyclerViewActions.actionOnItemAtPosition(1, new DeleteViewAction()));
         // Then : the number of element is 11
-        onView(allOf(ViewMatchers.withId(R.id.list_neighbours), hasDescendant(withText("Vincent")))).check(withItemCount(ITEMS_COUNT - 1));
+        onView(allOf(ViewMatchers.withId(R.id.list_neighbours), isDisplayed())).check(withItemCount(ITEMS_COUNT - 1));
     }
 
     ////My Instrumented Tests//////
 
     @Test
     public void neighboursList_clickOnRow_showDetailsWellFirstname() {
-        onView(allOf(ViewMatchers.withId(R.id.list_neighbours), hasDescendant(withText("Vincent"))))
+        onView(allOf(ViewMatchers.withId(R.id.list_neighbours), isDisplayed()))
 
                 .perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
         onView(ViewMatchers.withId(R.id.detail_user))
@@ -102,9 +107,9 @@ public class NeighboursListTest {
     @Test
     public void neighbourList_deleteOne_shouldHaveOneUserAtLeast() {
         //LIST COUNT 11 NEIGHBOURS NOW
-        onView(allOf(ViewMatchers.withId(R.id.list_neighbours),hasDescendant(withText("Vincent")))).check(withItemCount(ITEMS_COUNT - 1 ));
+        onView(allOf(ViewMatchers.withId(R.id.list_neighbours), isDisplayed())).check(withItemCount(ITEMS_COUNT - 1));
         //CLICK DELETE BUTTON AT POSITION 1 (CHLOE CAUSE JACK IS ALREADY DELETED)
-        onView(allOf(ViewMatchers.withId(R.id.list_neighbours),hasDescendant(withText("Vincent"))))
+        onView(allOf(ViewMatchers.withId(R.id.list_neighbours), isDisplayed()))
                 .perform(RecyclerViewActions.actionOnItemAtPosition(
                         1,
                         new ViewAction() {
@@ -128,33 +133,60 @@ public class NeighboursListTest {
                         })
                 );
         //TEST BEFORE DELETE USER YET SO NOW 11 USERS (ITEMS_COUNT - 2)
-        onView(allOf(ViewMatchers.withId(R.id.list_neighbours),hasDescendant(withText("Vincent")))).check(withItemCount(ITEMS_COUNT - 2));
+        onView(allOf(ViewMatchers.withId(R.id.list_neighbours), isDisplayed())).check(withItemCount(ITEMS_COUNT - 2));
     }
 
     @Test
     public void neighbourListFav_shouldShowOnlyFavNeighbour() {
         //LIST FAVORITE NEIGHBOUR EMPTY
-        onView(allOf(ViewMatchers.withId(R.id.list_neighbours),(not(isDisplayed()))))
+        onView(allOf(ViewMatchers.withId(R.id.list_neighbours), (not(isDisplayed()))))
                 .check(matches(hasChildCount(0)));
         //CLICK ON FIRST NEIGHBOUR (CAROLINE)
-        onView(allOf(ViewMatchers.withId(R.id.list_neighbours),hasDescendant(withText("Vincent"))))
+        onView(allOf(ViewMatchers.withId(R.id.list_neighbours), isDisplayed()))
                 .perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
 //        //ADD AS FAVORITE
         onView(ViewMatchers.withId(R.id.add_favorite)).perform(click());
 //        //GO BACK
         onView(ViewMatchers.withId(R.id.backArrow)).perform(click());
-//        //CHANGE TAB
-        onView(ViewMatchers.withId(R.id.container)).perform(swipeLeft());
-//        //LIST FAVORITE NEIGHBOUR COUNT ONE PERSON
-//        onView(allOf(ViewMatchers.withId(R.id.list_neighbours), isDisplayed()))
-//                .check(matches(hasChildCount(1)));
-        onView(allOf(ViewMatchers.withId(R.id.list_neighbours), hasChildCount(1)))
-                .check(matches(hasDescendant(withText("Caroline"))));
+        //CHANGE TAB
+
+        ViewInteraction tabView = onView(
+                Matchers.allOf(withContentDescription("Favorites"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.tabs),
+                                        0),
+                                1),
+                        isDisplayed()));
+        tabView.perform(click());
+
+        //LIST FAVORITE NEIGHBOUR COUNT ONE PERSON
+        onView(allOf(ViewMatchers.withId(R.id.list_neighbours), isDisplayed()))
+                .check(matches(hasChildCount(1)));
+
 //        //CLICK ON ALONE NEIGHBOUR
-        onView(allOf(ViewMatchers.withId(R.id.list_neighbours),hasChildCount(1)))
+        onView(allOf(ViewMatchers.withId(R.id.list_neighbours), isDisplayed()))
                 .perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
 //        //CHECK IF FIRSTNAME IS CAROLINE
         onView(ViewMatchers.withId(R.id.firstname))
                 .check(matches(withText("Caroline")));
+    }
+
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
     }
 }
