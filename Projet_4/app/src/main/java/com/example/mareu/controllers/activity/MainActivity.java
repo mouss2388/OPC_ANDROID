8bca690859a850353cb911f2dc96ac4c30f09b66
mouss <mouss2388@gmail.com>
2022-01-08 22:26:47 +0100

    Creation menu into toolbar

--- Projet_4/app/src/main/java/com/example/mareu/controllers/activity/MainActivity.java
+++ Projet_4/app/src/main/java/com/example/mareu/controllers/activity/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.mareu.controllers.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.mareu.R;
 import com.example.mareu.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
@@ -23,6 +27,38 @@ public class MainActivity extends AppCompatActivity {
 
         this.configureToolbar();
         mBinding.btnAddReu.setOnClickListener(v -> btn_add());
+    }
+
+    ////////////MENU/////////////
+
+    /**
+     * @param menu which replace by menu_activity_main
+     * @return
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_activity_main, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+
+            case R.id.filter_date:
+                Log.i(TAG, "click on date");
+                return true;
+            case R.id.filter_place:
+                Log.i(TAG, "click on place");
+                return true;
+            case R.id.filter_reset:
+                Log.i(TAG, "click on reset");
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     private void configureToolbar() {
