5afc4f59bc282b3ec3c21d01a4f90eedb556579f
mouss <mouss2388@gmail.com>
2022-02-04 16:37:11 +0100

    Add Unit Test for get, add, delete and filter meeting

--- Projet_4/app/src/test/java/com/example/mareu/MeetingServiceTest.java
+++ Projet_4/app/src/test/java/com/example/mareu/MeetingServiceTest.java
@@ -1,15 +1,24 @@
 package com.example.mareu;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
 import static org.junit.Assert.*;
+
+import com.example.mareu.model.Meeting;
+import com.example.mareu.service.MeetingApiService;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
-public class ExampleUnitTest {
+@RunWith(JUnit4.class)
+public class MeetingServiceTest {
+
+    private MeetingApiService service;
+    private Meeting meeting;
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
