b06980813d6de9d81582c96933aff8ad00c9d8b0
mouss <mouss2388@gmail.com>
2022-06-27 17:09:56 +0200

    Authentification with email works

--- Projet_7/app/src/main/java/com/example/projet_7/MainActivity.java
+++ Projet_7/app/src/main/java/com/example/projet_7/MainActivity.java
@@ -1,15 +1,25 @@
 package com.example.projet_7;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Toast;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+
 import com.example.projet_7.databinding.ActivityMainBinding;
+import com.firebase.ui.auth.AuthUI;
 import com.google.android.material.snackbar.Snackbar;
+
+import java.util.Collections;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+    private ActivityResultLauncher<Intent> startForResult;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +29,28 @@ public class MainActivity extends AppCompatActivity {
         View view = binding.getRoot();
         setContentView(view);
 
+        this.handleResponseAfterSignIn();
         this.setupListeners();
+    }
+
+    private void handleResponseAfterSignIn() {
+        startForResult = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
+
+            if (result.getResultCode() == RESULT_OK) {
+//                userManager.createUser();
+                showSnackBar(getResources().getString(R.string.snackbar_msg_login_success));
+
+            } else if (result.getResultCode() == RESULT_CANCELED) {
+                showSnackBar(getResources().getString(R.string.snackbar_msg_login_cancelled));
+            } else {
+                showSnackBar(getResources().getString(R.string.snackbar_msg_error_unknow));
+            }
+        });
+    }
+
+    // Show Snack Bar with a message
+    private void showSnackBar(String message) {
+        Snackbar.make(binding.mainLayout, message, Snackbar.LENGTH_SHORT).show();
     }
 
     private void setupListeners() {
@@ -31,12 +62,33 @@ public class MainActivity extends AppCompatActivity {
         binding.bntLoginGoogle.setOnClickListener(v ->
                 showSnackBar(getResources().getString(R.string.snackbar_msg_login_success) + " Google"));
 
-        binding.bntLoginEmail.setOnClickListener(v ->
-                        showSnackBar(getResources().getString(R.string.snackbar_msg_login_success) + " Email"));
+        binding.bntLoginEmail.setOnClickListener(v -> signInWith("email"));
+
     }
 
-    // Show Snack Bar with a message
-    private void showSnackBar(String message) {
-        Snackbar.make(binding.mainLayout, message, Snackbar.LENGTH_SHORT).show();
+
+    private void signInWith(String provider) {
+        //AuthUI provider
+        AuthUI.IdpConfig authProvider;
+
+        // Choose authentication providers
+        switch (provider) {
+            case "facebook":
+                authProvider = new AuthUI.IdpConfig.FacebookBuilder().build();
+                break;
+            case "google":
+                authProvider = new AuthUI.IdpConfig.GoogleBuilder().build();
+                break;
+            default:
+                authProvider = new AuthUI.IdpConfig.EmailBuilder().build();
+                break;
+        }
+
+        Intent intentSignIn = AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(Collections.singletonList(authProvider))
+                .setIsSmartLockEnabled(false, true)
+                .build();
+        startForResult.launch(intentSignIn);
     }
 }
