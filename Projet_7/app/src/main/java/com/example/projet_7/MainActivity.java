b622f344add3511fd725498e8f21cb8f0119c783
mouss <mouss2388@gmail.com>
2022-06-27 11:11:22 +0200

    Add Email login button  and show toast when it is clicked

--- Projet_7/app/src/main/java/com/example/projet_7/MainActivity.java
+++ Projet_7/app/src/main/java/com/example/projet_7/MainActivity.java
@@ -1,13 +1,13 @@
 package com.example.projet_7;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
-
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
 import com.example.projet_7.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
     private ActivityMainBinding binding;
 
     @Override
@@ -16,5 +16,11 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
+
+        this.setupListeners();
+    }
+
+    private void setupListeners() {
+        binding.bntLoginEmail.setOnClickListener(v -> Toast.makeText(this, R.string.snackbar_msg_login_success, Toast.LENGTH_SHORT).show());
     }
 }
