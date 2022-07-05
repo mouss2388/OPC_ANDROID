5e1140db59c942cec5a714419399bd239672c6e6
mouss <mouss2388@gmail.com>
2022-07-05 12:27:46 +0200

    Create User Model and add firestore to project

---
+++ Projet_7/app/src/main/java/com/example/projet_7/model/User.java
@@ -0,0 +1,33 @@
+package com.example.projet_7.model;
+
+import androidx.annotation.Nullable;
+
+public class User {
+
+    private String uid;
+    private String username;
+    @Nullable
+    private String urlPicture;
+
+    public User() { }
+
+    public User(String uid, String username, @Nullable String urlPicture) {
+        this.uid = uid;
+        this.username = username;
+        this.urlPicture = urlPicture;
+    }
+
+
+
+    // --- GETTERS ---
+    public String getUid() { return uid; }
+    public String getUsername() { return username; }
+    @Nullable
+    public String getUrlPicture() { return urlPicture; }
+
+    // --- SETTERS ---
+    public void setUsername(String username) { this.username = username; }
+    public void setUid(String uid) { this.uid = uid; }
+    public void setUrlPicture(@Nullable String urlPicture) { this.urlPicture = urlPicture; }
+
+}
