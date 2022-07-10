dbacd0ea18b0b2075fc6e36a9233beb4b6d99f2c
mouss <mouss2388@gmail.com>
2022-07-10 22:30:06 +0200

    Get and zoom on current location
    - Create callback in order to get request location update if last location is null

--- Projet_7/app/src/main/java/com/example/projet_7/ui/maps/MapsFragment.java
+++ Projet_7/app/src/main/java/com/example/projet_7/ui/maps/MapsFragment.java
@@ -6,38 +6,52 @@ import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Looper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import com.example.projet_7.R;
 import com.example.projet_7.databinding.FragmentMapsBinding;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.Priority;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.tasks.Task;
 
 @SuppressWarnings("MissingPermission")
 public class MapsFragment extends Fragment implements OnMapReadyCallback, GoogleMap.OnMyLocationButtonClickListener,
-        GoogleMap.OnMyLocationClickListener  {
+        GoogleMap.OnMyLocationClickListener {
 
     private FragmentMapsBinding binding;
-    private Context context;
-    private SupportMapFragment mapFragment;
+    private final Context context;
+    private SupportMapFragment supportMapFragment;
 
     private String[] PERMISSIONS;
-    private ActivityResultLauncher<String[]> requestPermissionLauncher = null;
+    private ActivityResultLauncher<String[]> requestPermissionLauncher;
 
+    private Location currentLocation;
+    private FusedLocationProviderClient mFusedLocationClient;
 
+    private LocationRequest mLocationRequest = null;
+    private LocationCallback mLocationCallback = null;
+
+    private  GoogleMap googleMap;
 
 
     public MapsFragment(Context context) {
@@ -51,7 +65,6 @@ public class MapsFragment extends Fragment implements OnMapReadyCallback, Google
         return binding.getRoot();
     }
 
-    @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
@@ -59,7 +72,7 @@ public class MapsFragment extends Fragment implements OnMapReadyCallback, Google
         this.handleResponsePermissionsRequest();
 
         if (this.isAndroidVersionBelowMarshmallow()) {
-            Toast.makeText(context, "you already have permissions", Toast.LENGTH_SHORT).show();
+            fetchLocation();
         } else {
             checkPermissions();
         }
@@ -73,6 +86,34 @@ public class MapsFragment extends Fragment implements OnMapReadyCallback, Google
                 Manifest.permission.ACCESS_COARSE_LOCATION,
         };
 
+        currentLocation = null;
+        requestPermissionLauncher = null;
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this.context.getApplicationContext());
+        mLocationRequest = null;
+        mLocationCallback = null;
+        googleMap = null;
+
+        initCallBackLocation();
+        createLocationRequest();
+    }
+
+    private void initCallBackLocation() {
+        mLocationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(@NonNull LocationResult locationResult) {
+                super.onLocationResult(locationResult);
+                currentLocation = locationResult.getLastLocation();
+            }
+        };
+    }
+
+    private void createLocationRequest() {
+
+        long LOCATION_REQUEST_INTERVAL = 1000L;
+
+        mLocationRequest = LocationRequest.create();
+        mLocationRequest.setPriority(Priority.PRIORITY_HIGH_ACCURACY)
+                .setInterval(LOCATION_REQUEST_INTERVAL);
     }
 
     private void handleResponsePermissionsRequest() {
@@ -98,10 +139,38 @@ public class MapsFragment extends Fragment implements OnMapReadyCallback, Google
         return Build.VERSION.SDK_INT < Build.VERSION_CODES.M;
     }
 
+    private void fetchLocation() {
+
+        Task<Location> task = mFusedLocationClient.getLastLocation();
+        task.addOnSuccessListener(location -> {
+            if (location != null) {
+                currentLocation = location;
+                Toast.makeText(getActivity(), currentLocation.getLatitude() + "" + currentLocation.getLongitude(), Toast.LENGTH_SHORT).show();
+
+                supportMapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.maps);
+                assert supportMapFragment != null;
+                supportMapFragment.getMapAsync(MapsFragment.this);
+
+            }else {
+                requestLocationUpdate();
+                fetchLocation();
+            }
+        });
+    }
+
+    private void requestLocationUpdate() {
+
+        mFusedLocationClient.requestLocationUpdates(
+                mLocationRequest,
+                mLocationCallback,
+                Looper.myLooper()
+        );
+    }
+
     private void checkPermissions() {
 
         if (hasPermissions()) {
-            ///Start location
+            fetchLocation();
         } else {
             askLocationPermissions();
         }
@@ -122,11 +191,31 @@ public class MapsFragment extends Fragment implements OnMapReadyCallback, Google
     private void askLocationPermissions() {
         requestPermissionLauncher.launch(PERMISSIONS);
     }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        this.requestLocationUpdate();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mFusedLocationClient.removeLocationUpdates(mLocationCallback);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mFusedLocationClient.removeLocationUpdates(mLocationCallback);
+    }
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+        mFusedLocationClient.removeLocationUpdates(mLocationCallback);
     }
+
 
     @Override
     public boolean onMyLocationButtonClick() {
@@ -144,8 +233,16 @@ public class MapsFragment extends Fragment implements OnMapReadyCallback, Google
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
 
-        googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-        googleMap.setOnMyLocationButtonClickListener(this);
-        googleMap.setOnMyLocationClickListener(this);
+        this.googleMap = googleMap;
+        this.googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        this.googleMap.setOnMyLocationButtonClickListener(this);
+        this.googleMap.setOnMyLocationClickListener(this);
+        this.googleMap.getUiSettings().setZoomControlsEnabled(true);
+        this.googleMap.getUiSettings().setMyLocationButtonEnabled(true);
+        this.googleMap.setMyLocationEnabled(true);
+
+        LatLng latLng = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
+        this.googleMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+        this.googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 17));
     }
 }
