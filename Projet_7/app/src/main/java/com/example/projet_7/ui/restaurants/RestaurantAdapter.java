cb15c9ad1192d5165e92af3856fb525f4b5f3c83
mouss <mouss2388@gmail.com>
2022-07-22 23:07:09 +0200

    Customize different dimensions of restaurants list

--- Projet_7/app/src/main/java/com/example/projet_7/ui/restaurants/RestaurantAdapter.java
+++ Projet_7/app/src/main/java/com/example/projet_7/ui/restaurants/RestaurantAdapter.java
@@ -3,7 +3,6 @@ package com.example.projet_7.ui.restaurants;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -11,17 +10,15 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.projet_7.R;
 import com.example.projet_7.model.Restaurant;
-import com.example.projet_7.ui.MainActivity;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class RestaurantAdapter extends RecyclerView.Adapter<RestaurantAdapter.ViewHolder> {
 
     private final ArrayList<Restaurant> restaurants;
 
     public RestaurantAdapter(ArrayList<Restaurant> restaurants) {
-        this.restaurants = (ArrayList<Restaurant>) restaurants;
+        this.restaurants = restaurants;
 
     }
 
@@ -50,25 +47,13 @@ public class RestaurantAdapter extends RecyclerView.Adapter<RestaurantAdapter.Vi
 
         private final TextView name;
         private final TextView address;
-        private final TextView type;
-        private final TextView opening_hours;
-        private final ImageView photo;
-        private final TextView distance;
-        private final TextView total_rating;
-
 
 
         public ViewHolder(View view) {
             super(view);
             // Define click listener for the ViewHolder's View
-            name =  view.findViewById(R.id.name);
-            address= view.findViewById(R.id.address);
-            type =  view.findViewById(R.id.type_Of_Restaurant);
-            opening_hours =  view.findViewById(R.id.opening_hours);
-            distance =  view.findViewById(R.id.distance_to_restaurant);
-            total_rating = view.findViewById(R.id.total_rating);
-            photo = view.findViewById(R.id.photo_restaurant);
-
+            name = view.findViewById(R.id.name_Restaurant);
+            address = view.findViewById(R.id.address_Restaurant);
         }
 
         public void displayRestaurant(Restaurant restaurant) {
@@ -76,10 +61,6 @@ public class RestaurantAdapter extends RecyclerView.Adapter<RestaurantAdapter.Vi
             address.setText(restaurant.getAddress());
         }
     }
-
-
-
-
 
 
 }
