6a22bf6a3d4b2f11f9b435e45e15f4f1b9c4cc1f
mouss <mouss2388@gmail.com>
2022-09-06 14:34:14 +0200

    Add test

--- Projet_7/app/src/androidTest/java/com/example/projet_7/ExampleInstrumentedTest.java
+++ Projet_7/app/src/androidTest/java/com/example/projet_7/ExampleInstrumentedTest.java
@@ -1,14 +1,35 @@
 package com.example.projet_7;
 
-import android.content.Context;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
+import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.is;
 
-import androidx.test.platform.app.InstrumentationRegistry;
+import android.arch.core.executor.testing.InstantTaskExecutorRule;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.espresso.matcher.ViewMatchers;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import com.example.projet_7.ui.LoginActivity;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import static org.junit.Assert.*;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -17,10 +38,117 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+
+    @Rule
+    public InstantTaskExecutorRule instantTaskExecutorRule = new InstantTaskExecutorRule();
+
+    @Rule
+    public ActivityScenarioRule<LoginActivity> rule = new ActivityScenarioRule<>(LoginActivity.class);
+
+    private View decorView;
+
+    @Before
+    public void setUp() {
+        rule.getScenario().onActivity(activity -> decorView = activity.getWindow().getDecorView());
+        rule.getScenario().recreate();
+    }
+
+
     @Test
-    public void useAppContext() {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.projet_7", appContext.getPackageName());
+    public void loginWithSuccess() {
+
+        onView((withId(R.id.login_layout))).check(matches(isDisplayed()));
+        onView(withId(R.id.bnt_login_google)).check(matches(isDisplayed()))
+                .perform(click());
+
+        try {
+            Thread.sleep(7000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        onView((withId(R.id.main_Layout))).check(matches(isDisplayed()));
+
+        ViewInteraction appCompatImageButton = onView(
+                allOf(withContentDescription("Open navigation drawer"),
+                        childAtPosition(
+                                allOf(withId(R.id.activity_main_toolbar),
+                                        childAtPosition(
+                                                withClassName(is("android.widget.LinearLayout")),
+                                                0)),
+                                2),
+                        isDisplayed()));
+        appCompatImageButton.perform(click());
+
+        onView(withId(R.id.user_Email)).check(matches(ViewMatchers.withText("mouss2388@gmail.com")));
+    }
+
+
+    @Test
+    public void loginActivityTest() {
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        try {
+            Thread.sleep(7000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        ViewInteraction materialButton = onView(
+                allOf(withId(R.id.bnt_login_google), withText("Sign in with Google"),
+                        childAtPosition(
+                                allOf(withId(R.id.linearyout_buttons),
+                                        childAtPosition(
+                                                withId(R.id.login_layout),
+                                                1)),
+                                1),
+                        isDisplayed()));
+        materialButton.perform(click());
+
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        try {
+            Thread.sleep(7000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        ViewInteraction bottomNavigationItemView = onView(
+                allOf(withId(R.id.navigation_restaurants), withContentDescription("List View"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.bottom_nav_view),
+                                        0),
+                                1),
+                        isDisplayed()));
+        bottomNavigationItemView.perform(click());
+
+        ViewInteraction recyclerView = onView(
+                allOf(withId(R.id.recyclerview),
+                        childAtPosition(
+                                withId(R.id.top_of_recyclerView),
+                                0)));
+//          recyclerView.perform(actionOnItemAtPosition(0, click()));
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
     }
 }
--- Projet_7/app/src/test/java/com/example/projet_7/ExampleUnitTest.java
+++ Projet_7/app/src/test/java/com/example/projet_7/ExampleUnitTest.java
@@ -1,8 +1,13 @@
 package com.example.projet_7;
 
-import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
-import static org.junit.Assert.*;
+import com.example.projet_7.manager.UserManager;
+import com.example.projet_7.model.User;
+import com.google.android.gms.tasks.Task;
+
+import org.junit.Test;
+import org.mockito.Mockito;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -11,7 +16,16 @@ import static org.junit.Assert.*;
  */
 public class ExampleUnitTest {
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void getUsersData() {
+        UserManager userManager = Mockito.mock(UserManager.class);
+        Task task = Mockito.mock(Task.class);
+
+        User user = new User();
+
+        Mockito.when(userManager.getUserData()).thenReturn(task);
+
+        Mockito.when(task.getResult()).thenReturn(user);
+
+        assertEquals(user, userManager.getUserData().getResult());
     }
 }
