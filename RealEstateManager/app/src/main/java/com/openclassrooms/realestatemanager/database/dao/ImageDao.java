fb1a14997b454b605b3e5f644b9f6ee29685885d
mouss <mouss2388@gmail.com>
2022-12-30 15:32:08 +0100

    Add images into Database

--- RealEstateManager/app/src/main/java/com/openclassrooms/realestatemanager/database/dao/ImageDao.java
+++ RealEstateManager/app/src/main/java/com/openclassrooms/realestatemanager/database/dao/ImageDao.java
@@ -2,9 +2,12 @@ package com.openclassrooms.realestatemanager.database.dao;
 
 import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
 import androidx.room.Query;
 
 import com.openclassrooms.realestatemanager.database.model.Image;
+import com.openclassrooms.realestatemanager.database.model.RealEstate;
 
 import java.util.List;
 
@@ -14,4 +17,9 @@ public interface ImageDao {
     @Query("SELECT * FROM image_table WHERE realEstateId =:idRealEstate")
     LiveData<List<Image>> getRealEstatesImages(long idRealEstate);
 
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    void insertAll(List<Image> images);
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    long insert(Image image);
 }
